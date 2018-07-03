7cb227ead7d0357fa0bd456331bdd067fe77c5a7
Eduardo Maia <ehammo@cin.ufpe.br>
2018-07-03 14:36:39 -0300

    Adapter now is passive view

--- app/src/main/java/com/ehammo/githubjavapop_mvp_clean/presentation/view/RepositoryContract.java
+++ app/src/main/java/com/ehammo/githubjavapop_mvp_clean/presentation/view/RepositoryContract.java
@@ -5,13 +5,18 @@ import com.ehammo.githubjavapop_mvp_clean.data.model.RepositoryCollection;
 
 public interface RepositoryContract {
 
-    interface Presenter {
+    interface BasePresenter {
         void attachView(RepositoryContract.View view);
         void dettachView(RepositoryContract.View view);
         void onResume();
         void onPause();
-        void onRepositoryChosen(Repository repository);
+        void onRefresh();
+    }
 
+    interface RepositoryPresenter extends BasePresenter{
+        void onBindRepositoryRowViewAtPosition(int position, RepositoryRowView rowView);
+        int getRepositoriesRowsCount();
+        void onRepositoryChosen(int position);
     }
 
     interface View {
---
+++ app/src/main/java/com/ehammo/githubjavapop_mvp_clean/presentation/view/RepositoryRowView.java
@@ -0,0 +1,11 @@
+package com.ehammo.githubjavapop_mvp_clean.presentation.view;
+
+import com.ehammo.githubjavapop_mvp_clean.data.model.Repository;
+
+public interface RepositoryRowView {
+
+    void setInfo(Repository repository);
+
+
+
+}
